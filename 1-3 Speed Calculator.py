0145fdcecf2d1195d6e2481d68a58272617094e6
Beeruitamsterdam <91442446+Beeruitamsterdam@users.noreply.github.com>
2021-11-29 21:11:35 +0100

    Create 1-3 Speed Calculator.py

---
+++ 1-3 Speed Calculator.py
@@ -0,0 +1,5 @@
+This is how the lines must be arranged:
+totalDistance = uphillDistance + downhillDistance
+totalTime = uphillTime + downhillTime
+averageSpeed = totalDistance / totalTime
+print(averageSpeed)
